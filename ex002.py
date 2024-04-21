740dc5c9d75f6b953dfa47e0544a64fc1ef3ebaa
unknown <fsilvanegro@hotmail.com>
2024-04-21 10:13:47 -0300

    mais uma fora de inserir a variavel já formatada dentro da frase

--- ex002.py
+++ ex002.py
@@ -1,3 +1,5 @@
 nome = input('Qual é o seu nome? ')
 
-print('Seja bem vindo,', nome,'.')
+#print('Seja bem vindo,', nome,'.')
+#outra forma de colocar a variável dentro de um lugar já formatado:
+print('Seja bem vindo, {}!'.format(nome))
