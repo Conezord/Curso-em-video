5d093c658c7259e30ea94e2762b4112137d986be
unknown <fsilvanegro@hotmail.com>
2024-04-24 23:02:57 -0300

    aula 7 operadores aritmeticos

---
+++ aula07.py
@@ -0,0 +1,14 @@
+#Operadores aritméticos
+# soma +
+# subtração -
+# multiplicação *
+# divisão /
+# exponenciação **
+# divisão exata //
+# resto de divisão %
+
+#Ordem de precedencia de contas
+# 1 - ()
+# 2 - ##
+# 3 - / e *
+# 4 - + e -
