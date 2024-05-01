f8d45070fb05a9b166daef0abaa203e07f128774
unknown <fsilvanegro@hotmail.com>
2024-05-01 18:52:56 -0300

    desafios 12 e 13 da aula 7, envolvendo dinheiro e porcentagens

---
+++ desafio012.py
@@ -0,0 +1,3 @@
+preco = float(input('Qual o preço do item? '))
+
+print('O item com desconto de 5% seria de {:.2f}'.format(preco * 0.95))
---
+++ desafio013.py
@@ -0,0 +1,3 @@
+salario = float(input("Qual o seu salário? "))
+
+print('Seu novo salário com aumento de 15% será de {:.2f} reais'.format(salario * 1.15))
