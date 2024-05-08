83891364750ba843404a9057e87b3ba48135181a
Conezord <fsilvanegro@hotmail.com>
2024-05-08 08:53:43 -0300

    ex 007 e 008

---
+++ ex007.py
@@ -0,0 +1,4 @@
+n1 = int(input('Qual a primeira nota? '))
+n2 = int(input('Qual a segunda nota? '))
+
+print('A primeira nota foi {:.1f}, a segunda {:.1f} e a média entre elas é {:.1f}'.format(n1, n2, (n1 + n2)/2))
