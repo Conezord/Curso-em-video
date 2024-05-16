998f2e21d512164172ab35a7ea34a247170dd932
Conezord <fsilvanegro@hotmail.com>
2024-05-15 21:24:43 -0300

    ex015

--- Exercicios/ex015.py
+++ Exercicios/ex015.py
@@ -1,2 +1,6 @@
 # Aluguel de carros
 
+dias = int(input('Por quantos dias foi alugado? '))
+km = float(input('Quantos km rodados? '))
+
+print('O total a pagar é R${:.2f}.'.format((dias * 60) + (km * 0.15)))
