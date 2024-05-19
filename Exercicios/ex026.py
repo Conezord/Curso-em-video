4b80be9299f9dbb73db6633ebbe2b3e1c52ab622
Conezord <fsilvanegro@hotmail.com>
2024-05-18 23:32:01 -0300

    ex26

---
+++ Exercicios/ex026.py
@@ -0,0 +1,7 @@
+# ler uma frase e quantas vezes aparece a letra A, primeira e ultima vez que aparece
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+
+print('A letra "a" aparece {} vezes na frase'.format(frase.count('A')))
+print('Ela aparece a primeira vez na posição {}'.format(frase.find('A')))
+print('Ela aparece pela última vez na posição {}'.format(frase.rfind('A')))
