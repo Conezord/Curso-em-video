b286c73ef4f40bc0c704187ef1e52724a4110e96
Conezord <fsilvanegro@hotmail.com>
2024-05-21 19:14:15 -0300

    Exercicios

---
+++ Exercicios/ex028.py
@@ -0,0 +1,14 @@
+#Jogo da Adivinhação 
+
+from random import randint
+
+computador = randint(0, 5)
+print('-=-' * 20)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('-=-' * 20)
+jogador = int(input('Em que número eu pensei? '))
+if jogador == computador:
+    print('Parabéns, você conseguiu me vencer')
+else:
+    print('Ganhei! Pensei no número {} e não no {}'.format(computador, jogador))
+
