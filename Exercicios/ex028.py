aba5ffdf73211ecc90e8c4c9c8c4453e9b99d067
Conezord <fsilvanegro@hotmail.com>
2024-05-21 22:02:09 -0300

    ajuste ex28 adição do sleep

--- Exercicios/ex028.py
+++ Exercicios/ex028.py
@@ -1,5 +1,5 @@
 #Jogo da Adivinhação 
-
+from time import sleep
 from random import randint
 
 computador = randint(0, 5)
@@ -7,6 +7,8 @@ print('-=-' * 20)
 print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
 print('-=-' * 20)
 jogador = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+sleep(2)
 if jogador == computador:
     print('Parabéns, você conseguiu me vencer')
 else:
