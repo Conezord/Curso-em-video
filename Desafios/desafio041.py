f082c0ffd08d0ca11d329eef67439d41389bbf72
Conezord <fsilvanegro@hotmail.com>
2024-05-30 15:39:53 -0300

    desafio 40 e 41 da aula 12

---
+++ Desafios/desafio041.py
@@ -0,0 +1,16 @@
+#categoria de natação de acordo com a idade
+import datetime
+nascimento = int(input('Qual o ano de nascimento do atleta? '))
+data = datetime.datetime.today().year
+ano = data - nascimento
+
+if ano <= 9:
+    print('Categoria MIRIM')
+elif 9 < ano <= 14:
+    print('Categoria INFANTIL')
+elif 14 < ano <= 19:
+    print('Categoria JUNIOR')
+elif 19 < ano <= 20:
+    print('Categoria SÊNIOR')
+else:
+    print('Categoria MASTER')
