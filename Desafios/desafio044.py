4a1ad67cf0595d869a8d0ea64c00864c3331b7c8
Conezord <fsilvanegro@hotmail.com>
2024-06-02 16:29:59 -0300

    ajuste lojas cone ex 44

--- Desafios/desafio044.py
+++ Desafios/desafio044.py
@@ -1,13 +1,24 @@
 #preço do produto considerando o metodo de pagamento
+print('=' * 20, 'LOJAS CONE', '='* 20)
+preco = float(input('Qual o preço total das compras? R$ '))
+pgto = int(input('''Qual o método de pagamento? 
+[ 1 ] - à vista no dinheiro ou cheque
+[ 2 ] - à vista no cartão
+[ 3 ] - 2x no cartão
+[ 4 ] - 3x ou mais
+Sua opção: '''))
 
-preco = float(input('Qual o preço do produto? R$'))
-pgto = str(input('Qual o método de pagamento? ')).upper()
-
-if pgto == "DINHEIRO" or "CHEQUE":
+if pgto == 1:
     print('O valor do produto ficou R${:.2f}'.format(preco * 0.9))
-elif pgto == "CARTÃO" or "CARTAO":
+elif pgto == 2:
     print('O valor do produto ficou R${:.2f}'.format(preco * 0.95))
-elif pgto == "2x no cartão":
+elif pgto == 3:
     print("o preço final é de R${}.".format(preco))
-elif pgto == "3x ou mais":
+    print('Cada parcela ficou R${}'.format(preco/ 2))
+elif pgto == 4:
+    parcelas = (int(input('Qual o número de parcelas? ')))
     print('O preço final é de R${}'.format(preco * 1.2))
+    print('Cada parcela ficou R${}'.format((preco * 1.2)/ parcelas))
+else:
+    total = preco
+    print('OPÇÃO INVÁLIDA de pagamento. Tente novamente')
