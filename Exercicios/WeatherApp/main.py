746571e1c6b74765ed534563e00d233e1e80e87e
Conezord <fsilvanegro@hotmail.com>
2024-08-29 09:50:11 -0300

    remoção da API hardcoded

--- Exercicios/WeatherApp/main.py
+++ Exercicios/WeatherApp/main.py
@@ -1,14 +1,21 @@
-import requests
 import json
+import requests
 from pprint import pprint
+import os
 
-API_Key = 'ea66cb5a64e820427e4a6514cea63507'
+config_path = os.path.join(os.path.dirname(__file__), 'config.json')
+
+with open(config_path) as config_file:
+    config = json.load(config_file)
+    API_Key = config['API_Key']
+
+
 city = str(input('Digite uma cidade: '))
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
+base_url = f"http://api.openweathermap.org/data/2.5/weather?appid={API_Key}&q={city}&units=metric"
 
 weather_data = requests.get(base_url).json()
 pprint(weather_data)
 temperatura = weather_data["main"]["temp"]
 celsius = temperatura - 273.15
 
-print("A Temperatura atual em {} é de {:.1f}ºC".format(city, celsius))
+print("A Temperatura atual em {} é de {:.1f}ºC".format(city, temperatura))
